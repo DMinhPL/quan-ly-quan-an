cb61df079927fe81dc617ae794c98142050fe36d
Minh Pham <152623010+maverick75dm@users.noreply.github.com>
2024-06-15 22:59:33 +0700

    :pencil: update schema validation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * A function that normalizes a path by removing leading slashes.
+ *
+ * @param {string} path - The path to be normalized.
+ * @return {string} The normalized path.
+ */
+export const normalizePath = (path: string) => {
+  return path.startsWith('/') ? path.slice(1) : path;
+};
