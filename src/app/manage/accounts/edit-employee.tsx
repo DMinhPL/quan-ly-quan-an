0211a2eb2776c43b5a0bbf1f161aa46c3a03ee62
Maverick <152623010+maverick75dm@users.noreply.github.com>
2025-02-19 22:34:17 +0700

    :boom: implement edit account 50

--- src/app/manage/accounts/edit-employee.tsx
+++ src/app/manage/accounts/edit-employee.tsx
@@ -13,11 +13,15 @@ import { Label } from '@/components/ui/label'
 import { UpdateEmployeeAccountBody, UpdateEmployeeAccountBodyType } from '@/schemaValidations/account.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Upload } from 'lucide-react'
-import { useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Switch } from '@/components/ui/switch'
+import { useGetAccount, useUpdateAccountMutation } from '@/queries/useAccount'
+import { useUploadMediaMutation } from '@/queries/useMedia'
+import { toast } from '@/components/ui/use-toast'
+import { handleErrorApi } from '@/lib/utils'
 
 export default function EditEmployee({
   id,
@@ -30,6 +34,12 @@ export default function EditEmployee({
 }) {
   const [file, setFile] = useState<File | null>(null)
   const avatarInputRef = useRef<HTMLInputElement | null>(null)
+  const { data } = useGetAccount({
+    id: id as number,
+    enabled: Boolean(id)
+  })
+  const updateAccountMutation = useUpdateAccountMutation()
+  const uploadMediaMutation = useUploadMediaMutation()
   const form = useForm<UpdateEmployeeAccountBodyType>({
     resolver: zodResolver(UpdateEmployeeAccountBody),
     defaultValues: {
@@ -41,6 +51,22 @@ export default function EditEmployee({
       changePassword: false
     }
   })
+
+  useEffect(() => {
+    if (data) {
+      const { name, email, avatar } = data.payload.data
+      form.reset({
+        name,
+        email,
+        avatar: avatar ?? undefined,
+        changePassword: form.getValues('changePassword'),
+        password: form.getValues('password'),
+        confirmPassword: form.getValues('confirmPassword')
+      })
+
+    }
+  }, [data, form])
+
   const avatar = form.watch('avatar')
   const name = form.watch('name')
   const changePassword = form.watch('changePassword')
@@ -51,6 +77,35 @@ export default function EditEmployee({
     return avatar
   }, [file, avatar])
 
+  const onSubmit = async (values: UpdateEmployeeAccountBodyType) => {
+    if (updateAccountMutation.isPending) return
+    try {
+      let body: UpdateEmployeeAccountBodyType & { id: number } = { id: id as number, ...values }
+      if (file) {
+        const formData = new FormData()
+        formData.append('file', file)
+        const uploadImageResult = await uploadMediaMutation.mutateAsync(formData)
+        const imageUrl = uploadImageResult.payload.data
+        body = {
+          ...body,
+          avatar: imageUrl,
+        }
+      }
+      const result = await updateAccountMutation.mutateAsync(body)
+      toast({
+        description: result.payload.message,
+      })
+      form.reset()
+      onSubmitSuccess?.()
+      setId(undefined)
+    }
+    catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+  }
   return (
     <Dialog
       open={Boolean(id)}
@@ -66,7 +121,10 @@ export default function EditEmployee({
           <DialogDescription>Các trường tên, email, mật khẩu là bắt buộc</DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='edit-employee-form'>
+          <form
+            noValidate
+            onSubmit={form.handleSubmit(onSubmit, console.log)}
+            className='grid auto-rows-max items-start gap-4 md:gap-8' id='edit-employee-form'>
             <div className='grid gap-4 py-4'>
               <FormField
                 control={form.control}
