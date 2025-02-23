1a3bb982deeb9e4e86c3a10a9920899b6695da11
Maverick <152623010+maverick75dm@users.noreply.github.com>
2025-02-23 20:45:17 +0700

    :art: add dishes manage UI 53

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,12 @@
-import { type ClassValue, clsx } from "clsx";
-import { UseFormSetError } from "react-hook-form";
-import { twMerge } from "tailwind-merge";
-import { EntityError } from "./http";
-import { toast } from "@/components/ui/use-toast";
+import { type ClassValue, clsx } from 'clsx';
+import { UseFormSetError } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
+import { EntityError } from './http';
+import { toast } from '@/components/ui/use-toast';
 import jwt from 'jsonwebtoken';
-import authApiRequest from "@/apiRequests/auth";
-import { TokenPayload } from "@/types/jwt.types";
+import authApiRequest from '@/apiRequests/auth';
+import { TokenPayload } from '@/types/jwt.types';
+import { DishStatus, TableStatus } from '@/constants/type';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -18,7 +19,7 @@ export function cn(...inputs: ClassValue[]) {
  * @return {string} The normalized path.
  */
 export const normalizePath = (path: string) => {
-  return path.startsWith("/") ? path.slice(1) : path;
+  return path.startsWith('/') ? path.slice(1) : path;
 };
 
 export const handleErrorApi = ({
@@ -33,73 +34,112 @@ export const handleErrorApi = ({
   if (error instanceof EntityError && setError) {
     error.payload.errors.forEach((item) => {
       setError(item.field, {
-        type: "server",
+        type: 'server',
         message: item.message,
       });
     });
   } else {
     toast({
-      title: "Lỗi",
-      description: error?.payload?.message ?? "Lỗi không xác định",
-      variant: "destructive",
+      title: 'Lỗi',
+      description: error?.payload?.message ?? 'Lỗi không xác định',
+      variant: 'destructive',
       duration: duration ?? 5000,
     });
   }
 };
 
-const isBrowser = typeof window !== "undefined";
+const isBrowser = typeof window !== 'undefined';
 
 export const getAccessTokenFromLocalStorage = () =>
-  isBrowser ? localStorage.getItem("accessToken") : null;
+  isBrowser ? localStorage.getItem('accessToken') : null;
 
 export const getRefreshTokenFromLocalStorage = () =>
-  isBrowser ? localStorage.getItem("refreshToken") : null;
+  isBrowser ? localStorage.getItem('refreshToken') : null;
 
 export const setAccessTokenToLocalStorage = (value: string) =>
-  isBrowser && localStorage.setItem("accessToken", value);
+  isBrowser && localStorage.setItem('accessToken', value);
 
 export const setRefreshTokenToLocalStorage = (value: string) =>
-  isBrowser && localStorage.setItem("refreshToken", value);
+  isBrowser && localStorage.setItem('refreshToken', value);
 
 export const removeTokensFromLocalStorage = () => {
-  isBrowser && localStorage.removeItem('accessToken')
-  isBrowser && localStorage.removeItem('refreshToken')
-}
+  isBrowser && localStorage.removeItem('accessToken');
+  isBrowser && localStorage.removeItem('refreshToken');
+};
 
 export const decodeToken = (token: string) => {
-  return jwt.decode(token) as TokenPayload
-}
-export const checkAndRefreshToken = async (param?: { onError?: () => void; onSuccess?: () => void }) => {
+  return jwt.decode(token) as TokenPayload;
+};
+export const checkAndRefreshToken = async (param?: {
+  onError?: () => void;
+  onSuccess?: () => void;
+}) => {
   // Không nên đưa logic lấy access và refresh token ra khỏi cái function `checkAndRefreshToken`
   // Vì để mỗi lần mà checkAndRefreshToken() được gọi thì chúng ta se có một access và refresh token mới
   // Tránh hiện tượng bug nó lấy access và refresh token cũ ở lần đầu rồi gọi cho các lần tiếp theo
-  const accessToken = getAccessTokenFromLocalStorage()
-  const refreshToken = getRefreshTokenFromLocalStorage()
+  const accessToken = getAccessTokenFromLocalStorage();
+  const refreshToken = getRefreshTokenFromLocalStorage();
   // Chưa đăng nhập thì cũng không cho chạy
-  if (!accessToken || !refreshToken) return
-  const decodedAccessToken = decodeToken(accessToken)
-  const decodedRefreshToken = decodeToken(refreshToken)
+  if (!accessToken || !refreshToken) return;
+  const decodedAccessToken = decodeToken(accessToken);
+  const decodedRefreshToken = decodeToken(refreshToken);
   // Thời điểm hết hạn của token là tính theo epoch time (s)
   // Còn khi các bạn dùng cú pháp new Date().getTime() thì nó sẽ trả về epoch time (ms)
-  const now = new Date().getTime() / 1000 - 1
+  const now = new Date().getTime() / 1000 - 1;
   // trường hợp refresh token hết hạn thì cho logout
   if (decodedRefreshToken.exp <= now) {
-    removeTokensFromLocalStorage()
-    return param?.onError && param.onError()
+    removeTokensFromLocalStorage();
+    return param?.onError && param.onError();
   }
   // Ví dụ access token của chúng ta có thời gian hết hạn là 10s
   // thì mình sẽ kiểm tra còn 1/3 thời gian (3s) thì mình sẽ cho refresh token lại
   // Thời gian còn lại sẽ tính dựa trên công thức: decodedAccessToken.exp - now
   // Thời gian hết hạn của access token dựa trên công thức: decodedAccessToken.exp - decodedAccessToken.iat
-  if (decodedAccessToken.exp - now < (decodedAccessToken.exp - decodedAccessToken.iat) / 3) {
+  if (
+    decodedAccessToken.exp - now <
+    (decodedAccessToken.exp - decodedAccessToken.iat) / 3
+  ) {
     // Gọi API refresh token
     try {
-      const res = await authApiRequest.refreshToken()
-      setAccessTokenToLocalStorage(res.payload.data.accessToken)
-      setRefreshTokenToLocalStorage(res.payload.data.refreshToken)
-      param?.onSuccess && param.onSuccess()
+      const res = await authApiRequest.refreshToken();
+      setAccessTokenToLocalStorage(res.payload.data.accessToken);
+      setRefreshTokenToLocalStorage(res.payload.data.refreshToken);
+      param?.onSuccess && param.onSuccess();
     } catch (error) {
-      param?.onError && param.onError()
+      param?.onError && param.onError();
     }
   }
-}
+};
+
+export const getVietnameseDishStatus = (
+  status: (typeof DishStatus)[keyof typeof DishStatus]
+) => {
+  switch (status) {
+    case DishStatus.Available:
+      return 'Có sẵn';
+    case DishStatus.Unavailable:
+      return 'Không có sẵn';
+    default:
+      return 'Ẩn';
+  }
+};
+
+export const formatCurrency = (number: number) => {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  }).format(number);
+};
+
+export const getVietnameseTableStatus = (
+  status: (typeof TableStatus)[keyof typeof TableStatus]
+) => {
+  switch (status) {
+    case TableStatus.Available:
+      return 'Có sẵn';
+    case TableStatus.Reserved:
+      return 'Đã đặt';
+    default:
+      return 'Ẩn';
+  }
+};
